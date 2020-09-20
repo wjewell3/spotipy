9231dc140bea39634836ba7b20209a41e1f148da
FYE7200 <FYE7200@CORPFYE7200MB.local>
2020-09-20 00:56:46 -0500

    updated server.py to run pyxtermjs so users can see code running in terminal

--- server.py
+++ server.py
@@ -1,19 +1,22 @@
-# https://stackoverflow.com/questions/57580411/storing-spotify-token-in-flask-session-using-spotipy
 from flask import Flask, render_template, redirect, request, session, make_response, session, Response, stream_with_context
 import spotipy
 import spotipy.util as util
-#from credentz import *
 import requests
 import os
-#from create_playlist import *
 import sys
-#import uwsgi
 import logging
 from flask_cors import CORS
 import time
 import subprocess
 import signal
 import io
+import schedule
+import threading
+import webbrowser
+
+def change_port():
+    webbrowser.open('http://127.0.0.1:8080/',new=0)
+    return
 
 def default_sigpipe():
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
@@ -83,28 +86,12 @@ def api_callback():
 
 @application.route("/user_input")
 def user_input():
-    return render_template("base.html")
-    # return """
-    #      <html><body>
-    #          <h2>Welcome to Will Jewell's Spotipy Playlist Creator</h2>
-    #          <form action="/create_playlist_">
-    #              <p>What's do you want to call your new playlist?</p>
-    #              <input type='text' name='playlist_name'><br>
-    #              <p>What genre score threshold do you want to use (default 20)?\n
-    #              The higher the threshold score, the fewer songs you will get but the songs will be more relevant. </p>
-    #              <input type='text' name='genre_score_thresh'><br>
-    #              <input type='submit' value='Continue'>
-    #          </form>
-    #      </body></html>
-    #      """ 
-         
+    return render_template("base.html")         
 
 @application.route('/create_playlist_')
 def create_playlist_():
     playlist_name = request.args.get('playlist_name')
     genre_score_thresh = request.args['genre_score_thresh']
-    #session['playlist_name'] = playlist_name
-    #session['genre_score_thresh'] = genre_score_thresh
     if float(genre_score_thresh) <= 0:
         return """
         <html><body>
@@ -117,61 +104,12 @@ def create_playlist_():
         </form>
         </body></html>"""
     else: 
-        def inner():
-            proc = subprocess.Popen(
-                f'python test.py {playlist_name} {genre_score_thresh}'
-                ,shell=True
-                ,stdout=subprocess.PIPE
-                ,universal_newlines=True
-                ,bufsize=-1
-                ,preexec_fn=default_sigpipe
-            )
-            for line in iter(proc.stdout.readline,''):
-                yield line.rstrip() + '<br/>\n'
-                proc.stdout.flush()
+        subprocess.Popen(f"pyxtermjs -p 8080 --command python --cmd-args='create_playlist.py '{playlist_name}' '{genre_score_thresh}''"
+        ,shell=True
+        )
+        time.sleep(1)
+        subprocess.run('open http://127.0.0.1:8080/', shell=True)
+    return 
 
-        return Response(inner(), mimetype='text/html') 
-        #"""
-        #<html><body>
-            #<h2>Thank You!</h2>
-            #<h3>Playlist Name {0}</h3>
-            #<p>Genre Score Threshold {1}</p>
-        #</body></html>
-        #""".format(playlist_name, genre_score_thresh)
-        #def inner():
-            #cmd = ['python create_playlist.py', f"{playlist_name}", f"{genre_score_thresh}"]
-            #print(cmd)
-            #env = {**os.environ}
-            #proc = subprocess.Popen(
-                #cmd,
-                ##['ls','-l'],         
-                #shell=True,
-                ##env=env,
-                #stdout=subprocess.PIPE)
-            #for line in iter(proc.stdout.readline,''):
-                ##time.sleep(1)                          
-                #yield str(line.rstrip()) + '<br/>\n'
-
-    #return Response(inner(), mimetype='text/html') 
-    #return redirect("yield")
-
-@application.route('/yield')
-def index():
-    def inner():
-        #cmd = f"['python create_playlist.py '{session['playlist_name']}', '{session['genre_score_thresh']'}']"
-        #print(cmd)
-        env = {**os.environ}
-        proc = subprocess.Popen(
-            #cmd,
-            ['ls','-l'],         
-            shell=True,
-            #env=env,
-            stdout=subprocess.PIPE)
-        for line in iter(proc.stdout.readline,''):
-            #time.sleep(1)                          
-            yield str(line.rstrip()) + '<br/>\n'
-
-    return Response(inner(), mimetype='text/html') 
-
-if __name__ == "__main__":
-    application.run(debug=True,threaded=True)
+if __name__ == '__main__':
+    application.run()
--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 import sys
+import time
 a = sys.argv[1]
 b = sys.argv[2]
 print('create playlist ')
 print(a)
+time.sleep(10)
 print('genre threshold')
 print(b)
